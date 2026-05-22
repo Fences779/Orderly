f78a3d0688bcdb9969ddb671142ba785702276b3
Xuanmin Zhou <life22756@gmail.com>
2026-05-23 04:17:35 +0800

    5/23 04:17

--- src/Orderly.App/Views/MainWindow.xaml.cs
+++ src/Orderly.App/Views/MainWindow.xaml.cs
@@ -7,11 +7,14 @@ namespace Orderly.App.Views;
 
 public partial class MainWindow : Window
 {
+    private System.Threading.CancellationTokenSource? _copyToastCts;
+
     public MainWindow(MainViewModel viewModel)
     {
         InitializeComponent();
         DataContext = viewModel;
         System.Windows.Application.Current.MainWindow = this;
+        viewModel.PropertyChanged += ViewModel_PropertyChanged;
     }
 
     protected override void OnClosing(CancelEventArgs e)
@@ -101,7 +104,47 @@ public partial class MainWindow : Window
     {
         if (DataContext is MainViewModel vm)
         {
-            vm.DismissStringNarrationDetailsForSession();
+            if (DetailPanel != null && DetailPanel.RenderTransform is System.Windows.Media.TranslateTransform tt)
+            {
+                var opacityAnim = new System.Windows.Media.Animation.DoubleAnimation
+                {
+                    From = 1.0,
+                    To = 0.0,
+                    Duration = new Duration(System.TimeSpan.FromSeconds(0.2)),
+                    EasingFunction = new System.Windows.Media.Animation.CubicEase { EasingMode = System.Windows.Media.Animation.EasingMode.EaseIn }
+                };
+
+                var translateAnim = new System.Windows.Media.Animation.DoubleAnimation
+                {
+                    From = 0.0,
+                    To = 40.0,
+                    Duration = new Duration(System.TimeSpan.FromSeconds(0.2)),
+                    EasingFunction = new System.Windows.Media.Animation.CubicEase { EasingMode = System.Windows.Media.Animation.EasingMode.EaseIn }
+                };
+
+                var sb = new System.Windows.Media.Animation.Storyboard();
+                System.Windows.Media.Animation.Storyboard.SetTarget(opacityAnim, DetailPanel);
+                System.Windows.Media.Animation.Storyboard.SetTargetProperty(opacityAnim, new PropertyPath("Opacity"));
+
+                System.Windows.Media.Animation.Storyboard.SetTarget(translateAnim, DetailPanel);
+                System.Windows.Media.Animation.Storyboard.SetTargetProperty(translateAnim, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.X)"));
+
+                sb.Children.Add(opacityAnim);
+                sb.Children.Add(translateAnim);
+
+                sb.Completed += (s, ev) =>
+                {
+                    vm.DismissStringNarrationDetailsForSession();
+                    DetailPanel.BeginAnimation(UIElement.OpacityProperty, null);
+                    tt.BeginAnimation(System.Windows.Media.TranslateTransform.XProperty, null);
+                };
+
+                sb.Begin();
+            }
+            else
+            {
+                vm.DismissStringNarrationDetailsForSession();
+            }
         }
     }
 
@@ -144,6 +187,68 @@ public partial class MainWindow : Window
         }
 
         vm.CommitDeferredSettingsAutoSave();
+    }
+
+    private void ViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(MainViewModel.StringNarrationStatusMessage))
+        {
+            if (DataContext is MainViewModel vm && !string.IsNullOrEmpty(vm.StringNarrationStatusMessage) && vm.StringNarrationStatusMessage.StartsWith("已复制"))
+            {
+                ShowCopyToast(vm.StringNarrationStatusMessage);
+            }
+        }
+    }
+
+    private void ShowCopyToast(string message)
+    {
+        _copyToastCts?.Cancel();
+        _copyToastCts = new System.Threading.CancellationTokenSource();
+        var token = _copyToastCts.Token;
+
+        Text_CopyToast.Text = message;
+        Popup_CopyToast.IsOpen = true;
+
+        System.Threading.Tasks.Task.Delay(1500, token).ContinueWith(t =>
+        {
+            if (!token.IsCancellationRequested)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Popup_CopyToast.IsOpen = false;
+                });
+            }
+        }, token);
+    }
+
+    private void StepperNode_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    {
+        if (sender is FrameworkElement element && DataContext is MainViewModel vm)
+        {
+            var targetStatus = element.Tag as string;
+            if (!string.IsNullOrEmpty(targetStatus) && vm.SelectedStringNarrationOrderDetail is not null)
+            {
+                vm.StringNarrationFulfillmentStatusInput = targetStatus;
+                vm.UpdateStringNarrationFulfillmentCommand.Execute(null);
+            }
+        }
+    }
+
+    private void FulfillmentInput_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+    {
+        if (e.Key == System.Windows.Input.Key.Enter)
+        {
+            if (DataContext is MainViewModel vm && vm.UpdateStringNarrationFulfillmentCommand.CanExecute(null))
+            {
+                if (sender is System.Windows.Controls.TextBox textBox)
+                {
+                    var binding = textBox.GetBindingExpression(System.Windows.Controls.TextBox.TextProperty);
+                    binding?.UpdateSource();
+                }
+                vm.UpdateStringNarrationFulfillmentCommand.Execute(null);
+                e.Handled = true;
+            }
+        }
     }
 
     private static T? FindAncestor<T>(DependencyObject? source) where T : DependencyObject
