b358be99a8cc95ac531c82ad391c66b2f28365d5
Xuanmin Zhou <life22756@gmail.com>
2026-05-31 01:46:24 +0800

    refactor(ui): extract fulfillment statistics cards

---
+++ src/Orderly.App/Views/Sections/FulfillmentStatisticsCardsGroupA.xaml.cs
@@ -0,0 +1,9 @@
+namespace Orderly.App.Views.Sections;
+
+public partial class FulfillmentStatisticsCardsGroupA : System.Windows.Controls.UserControl
+{
+    public FulfillmentStatisticsCardsGroupA()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/Orderly.App/Views/Sections/FulfillmentStatisticsCardsGroupB.xaml.cs
@@ -0,0 +1,9 @@
+namespace Orderly.App.Views.Sections;
+
+public partial class FulfillmentStatisticsCardsGroupB : System.Windows.Controls.UserControl
+{
+    public FulfillmentStatisticsCardsGroupB()
+    {
+        InitializeComponent();
+    }
+}
